f51c7d29c8ab28507aa33e7ad1c3e7f98d381e25
Artem Levin <leoartemlev@yandex.ru>
2022-04-15 16:01:26 +0500

    Поиск в ширину

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Linq;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] Tilemap tilemap;
+
+    public Dictionary<Vector3Int, Vector3Int?> GetPath(Vector3 startPos)
+    {
+        Queue<Vector3Int> frontier = new Queue<Vector3Int>();
+        frontier.Enqueue(tilemap.WorldToCell(startPos));
+
+        Dictionary<Vector3Int, Vector3Int?> cameFrom = new Dictionary<Vector3Int, Vector3Int?>();
+        cameFrom.Add(tilemap.WorldToCell(startPos), null);
+
+        while (frontier.Count > 0)
+        {
+            Vector3Int currentPoint = frontier.Dequeue();
+            //Debug.Log("Current point: " + currentPoint);
+            foreach (var item in GetNeighbors(currentPoint))
+            {
+                if (!cameFrom.ContainsKey(item))
+                {
+                    //Debug.Log("Adding to came from: " + item);
+                    frontier.Enqueue(item);
+                    cameFrom.Add(item, currentPoint);
+                }
+            }
+        }
+
+        return cameFrom;
+    }
+
+    private Vector3Int[] GetNeighbors(Vector3Int point)
+    {
+        bool isEven = point.y % 2 == 0;
+
+        Vector3Int?[] neighbors =
+        {
+            point + new Vector3Int(+1, 0, 0),
+            point + new Vector3Int(0, -1, 0),
+            point + new Vector3Int(+1, -1, 0),
+            point + new Vector3Int(-1, 0, 0),
+            point + new Vector3Int(0, +1, 0),
+            point + new Vector3Int(isEven ? 0 : +1, +1, 0)
+        };
+
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            if (!tilemap.HasTile(neighbors[i].Value))
+                neighbors[i] = null;
+        }
+
+        return neighbors.Where(x => x != null).Select(x => x.Value).ToArray();
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] PathFinder finder;
+    [SerializeField] Transform unit;
+    [SerializeField] Vector3Int endPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var path = finder.GetPath(unit.position);
+        var current = path[endPoint];
+        Debug.Log(current);
+        while (current != null)
+        {
+            current = path[current.Value];
+            Debug.Log(current);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(tilemap.WorldToCell(worldPos));
+        }
+    }
+}
